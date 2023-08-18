f52dff7c6e46186005b5c7300d6bfefe8e7bb97e
Caleb German <Caleb.German@Bentley.com>
2023-08-18 13:11:33 -0500

    Added Lint Ignore Flags For Any And Deprecated Property

--- packages/test-app-frontend/src/main.tsx
+++ packages/test-app-frontend/src/main.tsx
@@ -2,6 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+// eslint-disable-next-line react/no-deprecated
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App/App";
@@ -10,4 +11,4 @@ import "@bentley/icons-generic-webfont/dist/bentley-icons-generic-webfont.css";
 import "@itwin/itwinui-layouts-css/styles.css";
 import "./index.css";
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root") as HTMLElement);
+render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root") as HTMLElement); // render is deprecated 
