4aa961253125080db786e36c18becf74dc620fff
roluk <70327485+roluk@users.noreply.github.com>
2024-05-21 14:52:14 +0300

    Move createViewState to its own file

--- packages/saved-views-react/src/experimental.ts
+++ packages/saved-views-react/src/experimental.ts
@@ -6,7 +6,6 @@ export { createSavedViewOptions, type CreateSavedViewOptionsParams } from "./Sav
 export { SavedViewsExpandableBlockWidget } from "./SavedViewsWidget/SavedViewsExpandableBlockWidget.js";
 export { SavedViewsFolderWidget } from "./SavedViewsWidget/SavedViewsFolderWidget.js";
 export type { LegacySavedViewBase } from "./api/utilities/SavedViewTypes.js";
-export {
-  applyExtensionsToViewport, createViewState,
-} from "./api/utilities/translation/SavedViewTranslation.js";
+export { applyExtensionsToViewport } from "./api/utilities/translation/SavedViewTranslation.js";
+export { createViewState } from "./createViewState.js";
 export { ModelCategoryOverrideProvider } from "./ui/viewlist/ModelCategoryOverrideProvider.js";
